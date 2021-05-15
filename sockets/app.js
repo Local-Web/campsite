97d94b007523c00131df472d31b661bcdcacf67e
Joe LeBlanc <contact@jlleblanc.com>
2021-05-15 17:51:33 +0000

    Rename "types" to "commands"

--- sockets/app.js
+++ sockets/app.js
@@ -1,6 +1,6 @@
 const WebSocket = require("ws");
 const { sendSocketMessage } = require("./messages/transport");
-const { types } = require("./messages/types");
+const { commands } = require("./messages/commands");
 const { parser } = require("./messages/parser");
 
 // Here's the new idea: add `messagesTypes` constant as a "dictionary", always pass the user state, 
@@ -19,8 +19,8 @@ exports.socketsApp = (server) => {
     // used to .get() the handler from messageTypes. Each messageTypes function is going
     // to return the new userState.
     const handleMessage = (message) => {
-      if (types.has(message.command)) {
-        state = types.get(message.command)({message, state});
+      if (commands.has(message.command)) {
+        state = commands.get(message.command)({message, state});
         // TODO: replace this with the actual fulfillment of the command. Function might get
         // passed back?
         sendSocketMessage(ws, `success: ${message.command} fulfilled!`);
--- sockets/messages/commands.js
+++ sockets/messages/commands.js
@@ -1,29 +1,29 @@
 // I should probably get this away from CJS long term, but this should work for now.
 
-let types = new Map();
+let commands = new Map();
 
 // `message` in these functions encompasses everything sent from the 
 // client to the server
-types.set('join', ({message, state}) => {
+commands.set('join', ({message, state}) => {
   // always return the user state. will probably want to return a fresh object here
   // rather than mutating the old one (depending on how FP we want to get.)
   return state;
 });
 
-types.set('agree', ({message, state}) => {
+commands.set('agree', ({message, state}) => {
   return state;
 });
 
-types.set('createRoom', ({message, state}) => {
+commands.set('createRoom', ({message, state}) => {
   return state;
 });
 
-types.set('enterRoom', ({message, state}) => {
+commands.set('enterRoom', ({message, state}) => {
   return state;
 });
 
-types.set('chat', ({message, state}) => {
+commands.set('chat', ({message, state}) => {
   return state;
 });
 
-exports.types = types;
+exports.commands = commands;
